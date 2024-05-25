3cc8f9209ebff9cd4d283f4a3b967a7d7239d462
AnishGarg1 <anishgarg805@gmail.com>
2024-05-25 23:51:14 +0530

    initial updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>VisualSort App</h1>
+      <h1 className='text-green-500'>VisualSort App</h1>
+      <Home/>/
     </div>
   );
 }
